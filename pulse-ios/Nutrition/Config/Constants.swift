3c95400c2cf1252eba91ac6eef295e064be1fd35
Khash <khashzd@gmail.com>
2026-05-06 20:41:27 -0400

    feat: constants for user key and storage locations

---
+++ pulse-ios/Nutrition/Config/Constants.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+enum Constants {
+    static let userKey = "khash"
+
+    enum Defaults {
+        static let baseURL = "nutrition.baseURL"
+    }
+
+    enum Keychain {
+        static let service = "com.khxsh.nutrition.apikey"
+        static let account = "default"
+    }
+}
